4c948e4822fb782dbef50932ea30a539c0575663
Lena Link <lenalink89@gmail.com>
2021-01-03 13:37:27 +0100

    frontend index styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "tasks", to: "tasks#index"
   post "tasks", to: "tasks#create"
-  get "tasks/new", to: "tasks#new", as: :new_task
   get "tasks/:id", to: "tasks#show", as: :task
   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
   patch "tasks/:id", to: "tasks#update"
